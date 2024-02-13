2212ccddc60be088774441128050681606771b45
unknown <meroo1995saggaf@outlook.com>
2024-02-13 12:23:59 +0300

    first commit

---
+++ WhatsappUI/WhatsappUI/Models/Call.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Essentials;
+
+namespace WhatsApp.Models
+{
+    /// <summary>
+    /// WhatsApp call class.
+    /// </summary>
+    public class Call
+    {
+        public int CallID { get; set; }
+        public Contact Contact { get; set; }
+        public string CallDate { get; set; }
+        public bool OutCall { get; set; }
+        public bool InCall { get; set; }
+        public bool IsMissedCall { get; set; }
+    }
+}
